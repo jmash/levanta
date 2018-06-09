e44e995340e2081611b149ac4fc66551cfcd962b
Jared Ashcraft <jaredmashcraft@gmail.com>
2018-06-08 21:56:02 -0400

    adds dummy config

---
+++ App/dummy_config.js
@@ -0,0 +1,8 @@
+// enter proper configuration and rename file to 'config.js'
+// real config shared only with deserving pairs of eyes! o_o
+
+var config = {
+    apiKey: '',
+    authDomain: '',
+    projectId: ''
+};
